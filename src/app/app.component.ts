9259f2b4acc0383cc1132a54d98801aef07692b4
Luscaw <luscaemanoel@gmail.com>
2022-08-14 13:13:33 -0300

    Projeto WilliArts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CepServiceService } from './cep-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  //title = 'projetoWilliArts';
+  constructor(private cepService: CepServiceService) { }
+
+  consultaCep(valor, form) {
+    this.cepService.buscar(valor).subscribe((dados) => this.populaForm(dados, form));
+  }
+
+  populaForm(dados, form) {
+    form.setValue({
+      cep: dados.cep,
+      logradouro: dados.logradouro,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      uf: dados.uf
+    });
+  }
 }
